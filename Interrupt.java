cc94e1f1d015a846a2c6ee310c08c4c8e2a04875
arqetype <arq@telenet.be>
2016-01-05 00:04:22 +0100

    Revert "Removed comment"
    This reverts commit 88065ea85c7587385ff3996cca0d5d3c98ae05ce.

--- Interrupt.java
+++ Interrupt.java
@@ -12,6 +12,7 @@ public class Interrupt {
 		Button.waitForAnyPress();
 		while(Button.readButtons()>0);
 		LCD.clear();
+//		LCD.clear();
 		Motor.A.rotateTo(1440,true);
 		while(Motor.A.isMoving()) {
 			Delay.msDelay(200);
