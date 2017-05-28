c68667fd5478abdb2fa97a94f077497a542f6989
arqetype <arq@telenet.be>
2017-05-29 00:07:17 +0200

    Small changes to basic setup

--- EV3-project/src/Startup/HelloEv3.java
+++ EV3-project/src/Startup/HelloEv3.java
@@ -6,17 +6,18 @@ import lejos.hardware.ev3.EV3;
 import lejos.hardware.BrickFinder;
 import lejos.hardware.Keys;
 import lejos.hardware.lcd.TextLCD;
-	public class HelloEv3 {
-		public static void main(String[] args) {
+
+public class HelloEv3 {
+	public static void main(String[] args) {
 		// get EV3 brick
 		EV3 ev3brick = (EV3) BrickFinder.getLocal();
-    	// instantized LCD class for displaying and Keys // class for buttons
-    	Keys buttons = ev3brick.getKeys();
-    	TextLCD lcddisplay = ev3brick.getTextLCD();
-    	// drawing text on the LCD screen based on
-    	// coordinates
-    	lcddisplay.drawString("Hello Ev3!", 2, 4);
-    	// exit program after any button pressed
-    	buttons.waitForAnyPress();
+		// instantized LCD class for displaying and Keys // class for buttons
+		Keys buttons = ev3brick.getKeys();
+		TextLCD lcddisplay = ev3brick.getTextLCD();
+		// drawing text on the LCD screen based on
+		// coordinates
+		lcddisplay.drawString("Hello Ev3!", 2, 4);
+		// exit program after any button pressed
+		buttons.waitForAnyPress();
 	}
 }
