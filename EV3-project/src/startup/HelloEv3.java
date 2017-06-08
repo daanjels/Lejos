3133b508ca62f3bfd0dd3bfd4fcfb31a002aeeac
arqetype <arq@telenet.be>
2017-06-08 23:19:49 +0200

    Renamed robot to wallE

--- EV3-project/src/startup/HelloEv3.java
+++ EV3-project/src/startup/HelloEv3.java
@@ -1,7 +1,6 @@
-package Startup;
+package startup;
 
-// import EV3 hardware packages for EV brick finding,
-// activating keys and LCD
+// import EV3 hardware packages for EV brick finding, activating keys and LCD
 import lejos.hardware.ev3.EV3;
 import lejos.hardware.BrickFinder;
 import lejos.hardware.Keys;
@@ -10,13 +9,13 @@ import lejos.hardware.lcd.TextLCD;
 public class HelloEv3 {
 	public static void main(String[] args) {
 		// get EV3 brick
-		EV3 ev3brick = (EV3) BrickFinder.getLocal();
-		// instantized LCD class for displaying and Keys // class for buttons
-		Keys buttons = ev3brick.getKeys();
-		TextLCD lcddisplay = ev3brick.getTextLCD();
+		EV3 wall_e = (EV3) BrickFinder.getLocal();
+		// instantiate LCD class for displaying and Keys class for buttons
+		Keys buttons = wall_e.getKeys();
+		TextLCD display = wall_e.getTextLCD();
 		// drawing text on the LCD screen based on
 		// coordinates
-		lcddisplay.drawString("Hello Ev3!", 2, 4);
+		display.drawString("Hello Eve!", 2, 4);
 		// exit program after any button pressed
 		buttons.waitForAnyPress();
 	}
