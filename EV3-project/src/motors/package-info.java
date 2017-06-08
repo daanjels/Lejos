1a72c9fd67f0939957ea4f0755ff12fcc2bfc227
arqetype <arq@telenet.be>
2017-06-08 23:26:57 +0200

    Added Package info

--- EV3-project/src/motors/package-info.java
+++ EV3-project/src/motors/package-info.java
@@ -2,9 +2,8 @@
  * The motor class provides access to the EV3 large servo motors. 
  * When controlling the movement of EV3, a motor must be connected to one of the four EV3 motor ports. 
  * The class provides an instance for each port: MotorPort.A, MotorPort.B, MotorPort.C, and MotorPort.D.
- */
-/**
  * @author Arqetype
+ * @version 1.0
  *
  */
 package motors;
