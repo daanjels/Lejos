1c8b39c3bdf6e2960156744e0d7445cada8004e6
arqetype <arq@telenet.be>
2017-06-16 23:28:25 +0200

    The City class with attributes for name, xPos and yPos

---
+++ EV3-project/src/route/City.java
@@ -0,0 +1,24 @@
+package route;
+
+/**
+ * 
+ * The City class represents a city on a map.
+ * It has a name and X and Y coordinates.
+ * cityname: the name of the city
+ * xPos: the x coordinate of the city
+ * yPos: the y coordinate of the city
+ * 
+ * @author Arqetype
+ *
+ */
+
+public class City {
+	String cityname;
+	int xPos, yPos;
+	
+	public City(String name, int x, int y) {
+		this.cityname = name;
+		this.xPos = x;
+		this.yPos = y;
+	}
+}
