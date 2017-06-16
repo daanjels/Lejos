4bdb551b96a76a3ea92b9b68570f2747c95fa0f7
arqetype <arq@telenet.be>
2017-06-16 23:52:39 +0200

    The Road class
    with from and to cities and the distance between the two.
    The dist method can calculate the distance using the coordinates of the
    cities.

---
+++ EV3-project/src/route/Road.java
@@ -0,0 +1,34 @@
+package route;
+
+/**
+ * The Road class represents a connection between two cities.
+ * It holds the two cities and the distance between them.
+ * A Road is always straight, so the distance can be calculated 
+ * via a method, using the coordinates of the city.
+ * 
+ * from: the city where the road starts from
+ * to: the city where the road goes to
+ * distance: the distance between the two cities
+ * dist: a method to calculate the distance between the two cities
+ * 
+ * @author Arqetype
+ *
+ */
+
+public class Road {
+	City from;
+	City to;
+	double distance;
+	
+	public Road(City from, City to) {
+		this.from = from;
+		this.to= to;
+		this.distance = dist(from.xPos, from.yPos, to.xPos, to.yPos);
+	}
+	
+	static double dist(int x1, int y1, int x2, int y2) {
+		double d = 0;
+		d = Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+		return d;
+	}
+}
