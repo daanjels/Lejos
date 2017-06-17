bf27907d32cf348eb6f294a9cb68cb65ab495d87
arqetype <arq@telenet.be>
2017-06-17 21:54:05 +0200

    Map class created
    Added properties nale, cities and roads
    Created method to add roads and cities
    Also overwritten toString method to return a map overview.

---
+++ EV3-project/src/route/Map.java
@@ -0,0 +1,51 @@
+package route;
+
+/**
+ * The Map class keeps lists of cities and roads. 
+ * It also contains several methods, the most important ones are to plan a route.
+ * cities: an array of cities
+ * roads: an array of roads between cities
+ * addCity - method to add a City to the map
+ * addRoad - method to add a Road to the map
+ * planRoute - method that builds a Route
+ * And then some helper methods
+ * 
+ * @author Arqetype
+ *
+ */
+
+import java.util.ArrayList;
+
+public class Map {
+	String mapname;
+	ArrayList<City> cities;
+	ArrayList<Road> roads;
+	
+	public Map(String name) {
+		mapname = name;
+		cities = new ArrayList<City>();
+		roads = new ArrayList<Road>();
+	}
+	
+	public void addCity(City city) {
+		cities.add(city);
+	}
+	
+	public void addRoad(Road road) {
+		roads.add(road);
+	}
+	
+	public String toString() {
+		String result = new String();
+		
+		result = "Map of " + this.mapname + "\n" + "Cities: \n";
+		for (int i = 0; i < this.cities.size(); i++) {
+			result = result + "- " + this.cities.get(i) + "\n";
+		}
+		result = result + "Roads:\n";
+		for (int i = 0; i < this.roads.size(); i++) {
+			result = result + "- " + this.roads.get(i) + "\n";
+		}
+		return result;
+	}
+}
--- EV3-project/src/route/Planner.java
+++ EV3-project/src/route/Planner.java
@@ -10,22 +10,30 @@ package route;
 public class Planner {
 
 	public static void main(String[] args) {
-		City A = new City("Antwerpen", 10, 20);
-		City B = new City("Brussel", 5, 60);
-		City C = new City("Charleroi", 20, 110);
+		City A = new City("The Shire", 10, 20);
+		City B = new City("Bree", 5, 60);
+		City C = new City("Rivendell", 20, 110);
+		City D = new City("Gondor", 60, 100);
 		
-		System.out.println("De steden zijn: " + A.cityname + ", " + B.cityname + " en " + C.cityname);
-		System.out.println("De middelste stad ligt op " + A.xPos + " : " + A.yPos);
-		System.out.println(C.cityname + " ligt op " + C.xPos + " : " + C.yPos);
-		
-		System.out.println(A.toString());
-		System.out.println(B.cityname + " heeft locatie " + B.locatie());
+		System.out.println("Cities and towns of Middle Earth: " + A.cityname + ", " 
+				+ B.cityname + ", " + C.cityname + " and " + D.cityname);
+		System.out.println("The town of the elves is at " + A.locatie());
+		System.out.println(D.cityname + " is situated here: " + C.locatie());
 		
 		Road AB = new Road(A, B);
 		Road BC = new Road(B, C);
 		
-		System.out.println("Van " + AB.from.cityname + " naar " + AB.to.cityname + " is " + AB.distance + "km.");
+		System.out.println("From " + AB.from.cityname + " to " + AB.to.cityname + " is " + AB.distance + "km.");
 		System.out.println(BC);
+		
+		Map middleEarth = new Map("Middle Earth");
+		middleEarth.addCity(A);
+		middleEarth.addCity(B);
+		middleEarth.addCity(C);
+		middleEarth.addRoad(BC);
+		middleEarth.addRoad(AB);
+		
+		System.out.println(middleEarth);
 	}
 
 }
