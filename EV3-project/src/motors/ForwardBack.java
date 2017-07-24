411ead527387c56cdf4f2ddb597929cf3768b709
arqetype <arq@telenet.be>
2017-07-24 10:56:19 +0200

    Added options to select which motor moves in which direction

--- EV3-project/src/motors/ForwardBack.java
+++ EV3-project/src/motors/ForwardBack.java
@@ -20,44 +20,45 @@ import lejos.utility.Delay;
 public class ForwardBack {
 
 	public static void main(String[] args) {
-		EV3LargeRegulatedMotor leftMotor = new EV3LargeRegulatedMotor(MotorPort.C);
+
 		EV3LargeRegulatedMotor rightMotor = new EV3LargeRegulatedMotor(MotorPort.B);
+		EV3LargeRegulatedMotor leftMotor = new EV3LargeRegulatedMotor(MotorPort.C);
 
 		// get EV3 brick
 		EV3 wallE = (EV3) BrickFinder.getLocal();
 
 		// instantiated LCD class for displaying and Keys class for buttons
 		Keys buttons = wallE.getKeys();
-		LCD.drawString("Druk op een knop", 0, 1);
-		LCD.drawString("om vooruit te gaan", 0, 2);
-		
-		// block the thread until a button is pressed
-		buttons.waitForAnyPress();
-
-		// move robot forward and display status on LCD change directions when button is pressed 
-		leftMotor.forward();
-		rightMotor.forward();
-		LCD.clear();
-		LCD.drawString("Vooruit!", 0, 0);
-		LCD.drawString("Druk op een knop", 0, 1);
-		LCD.drawString("om achteruit", 0, 2);
-		LCD.drawString("te gaan", 0, 3);
-
-		// block the thread until a button is pressed
-		buttons.waitForAnyPress();
-
-		// move robot backward and display status on LCD 
-		leftMotor.backward();
-		rightMotor.backward();
-		LCD.clear();
-		LCD.drawString("Achteruit!", 0, 0);
-		LCD.drawString("Druk op een knop", 0, 1);
-		LCD.drawString("om te stoppen", 0, 2);
-
-		// block the thread until a button is pressed
-		buttons.waitForAnyPress();
-
-		// stop robot and display status on LCD 
+		LCD.drawString("Motor tester", 0, 0);
+		LCD.drawString("Linkermotor:", 0, 1);
+		LCD.drawString("-LEFT -> vooruit", 0, 2);
+		LCD.drawString("-DOWN -> achteruit", 0, 3);
+		LCD.drawString("Rechtermotor:", 0, 4);
+		LCD.drawString("-RIGHT-> vooruit", 0, 5);
+		LCD.drawString("-UP   -> achteruit", 0, 6);
+		LCD.drawString("ESAPE -> verlaten", 0, 7);
+		while (buttons.getButtons() != Keys.ID_ESCAPE) {
+			if (buttons.getButtons() == Keys.ID_RIGHT) {
+				leftMotor.stop();
+				rightMotor.stop();
+				rightMotor.forward();
+			}
+			if (buttons.getButtons() == Keys.ID_LEFT) {
+				leftMotor.stop();
+				rightMotor.stop();
+				leftMotor.forward();
+			}
+			if (buttons.getButtons() == Keys.ID_UP) {
+				leftMotor.stop();
+				rightMotor.stop();
+				rightMotor.backward();
+			}
+			if (buttons.getButtons() == Keys.ID_DOWN) {
+				leftMotor.stop();
+				rightMotor.stop();
+				leftMotor.backward();
+			}
+		}
 		leftMotor.stop();
 		rightMotor.stop();
 		LCD.clear();
