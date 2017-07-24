d27522b2ef98276fd74542dc1ab310f7d440b4f6
arqetype <arq@telenet.be>
2017-07-24 11:57:51 +0200

    Some small changes to display

--- EV3-project/src/pilot/TriangleMover.java
+++ EV3-project/src/pilot/TriangleMover.java
@@ -1,8 +1,5 @@
 package pilot;
 
-import lejos.hardware.BrickFinder;
-import lejos.hardware.Keys;
-import lejos.hardware.ev3.EV3;
 import lejos.hardware.lcd.LCD;
 import lejos.hardware.motor.EV3LargeRegulatedMotor;
 import lejos.hardware.port.MotorPort;
@@ -10,24 +7,22 @@ import lejos.robotics.chassis.Chassis;
 import lejos.robotics.chassis.Wheel;
 import lejos.robotics.chassis.WheeledChassis;
 import lejos.robotics.navigation.*;
+import lejos.utility.Delay;
 
 public class TriangleMover {
 
-	static EV3LargeRegulatedMotor LEFT_MOTOR = new EV3LargeRegulatedMotor(MotorPort.C);
 	static EV3LargeRegulatedMotor RIGHT_MOTOR = new EV3LargeRegulatedMotor(MotorPort.B);
-	
+	static EV3LargeRegulatedMotor LEFT_MOTOR = new EV3LargeRegulatedMotor(MotorPort.C);	
 	
 	public static void main(String[] args) throws Exception {
-		EV3 wallE = (EV3) BrickFinder.getLocal();
-		Keys buttons = wallE.getKeys();
-		LCD.drawString("  Plotting out   ", 0, 0);
-		LCD.drawString("    a triangle   ", 0, 1);
-//		buttons.waitForAnyPress();
+		LCD.drawString("   Plotting out   ", 0, 2);
+		LCD.drawString("    a triangle    ", 0, 3);
+		LCD.drawString("taking right turns", 0, 4);
 		
 		// setup the wheel diameter of left (and right) motor in centimeters, i.e. 2.8 cm
 		// the offset number is the distance between the center of wheel to the center of robot, i.e. half of track width
-		Wheel wheel1 = WheeledChassis.modelWheel(LEFT_MOTOR, 3.22).offset(-9.2);
-		Wheel wheel2 = WheeledChassis.modelWheel(RIGHT_MOTOR, 3.22).offset(9.2);
+		Wheel wheel1 = WheeledChassis.modelWheel(RIGHT_MOTOR, 3.22).offset(9.2);
+		Wheel wheel2 = WheeledChassis.modelWheel(LEFT_MOTOR, 3.22).offset(-9.2).invert(true);
 		
 		// set up the chassis type, i.e. Differential pilot
 		Chassis chassis = new WheeledChassis(new Wheel[] {wheel1, wheel2},WheeledChassis.TYPE_DIFFERENTIAL);
@@ -39,22 +34,16 @@ public class TriangleMover {
 		
 		// loop 3 times to trace out a square
 		for (int sides = 0; sides < 3; sides++) {
-		pilot.travel(50);	// travel 50 centimeter
-		pilot.rotate(120);	// rotate 120 degrees
+			pilot.travel(50);	// travel 50 centimeter
+			pilot.rotate(120);	// rotate 120 degrees
+		}
 
-		}
-		// press the ESCAPE button to stop moving
-		while (pilot.isMoving()) {
-		if (buttons.getButtons() == Keys.ID_ESCAPE)
-		pilot.stop();
-		}
-		// block the thread until a button is pressed
 		LCD.clear();
-		LCD.drawString("..................", 0, 0);
-		LCD.drawString(".    Finished    .", 0, 4);
-		LCD.drawString("..................", 0, 8);
-		buttons.waitForAnyPress();
-		LEFT_MOTOR.close();
+		LCD.drawString("..................", 0, 2);
+		LCD.drawString(".    Finished    .", 0, 3);
+		LCD.drawString("..................", 0, 4);
+		Delay.msDelay(3000);
 		RIGHT_MOTOR.close();
+		LEFT_MOTOR.close();
 	}	
 }
