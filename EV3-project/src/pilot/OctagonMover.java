69b3bbd05fb12d84b34ca67b4f1a12d2ac01f3ff
arqetype <arq@telenet.be>
2017-07-24 11:40:33 +0200

    Improved display and replaced wait by delay

--- EV3-project/src/pilot/OctagonMover.java
+++ EV3-project/src/pilot/OctagonMover.java
@@ -10,23 +10,26 @@ import lejos.robotics.chassis.Chassis;
 import lejos.robotics.chassis.Wheel;
 import lejos.robotics.chassis.WheeledChassis;
 import lejos.robotics.navigation.*;
+import lejos.utility.Delay;
 
 public class OctagonMover {
 
-	static EV3LargeRegulatedMotor LEFT_MOTOR = new EV3LargeRegulatedMotor(MotorPort.C);
 	static EV3LargeRegulatedMotor RIGHT_MOTOR = new EV3LargeRegulatedMotor(MotorPort.B);
+	static EV3LargeRegulatedMotor LEFT_MOTOR = new EV3LargeRegulatedMotor(MotorPort.C);
 	
 	public static void main(String[] args) throws Exception {
 		EV3 wallE = (EV3) BrickFinder.getLocal();
 		Keys buttons = wallE.getKeys();
 //		LCD.drawString("0123456789abcdefgh")
-		LCD.drawString("  Plotting out   ", 0, 0);
-		LCD.drawString("   an octagon    ", 0, 1);
-		
+		LCD.drawString("   Plotting out   ", 0, 0);
+		LCD.drawString("    an octagon    ", 0, 1);
+		LCD.drawString("taking right turns", 0, 2);
+		Delay.msDelay(1000);
+
 		// setup the wheel diameter of left (and right) motor in centimeters, i.e. 2.8 cm
 		// the offset number is the distance between the center of wheel to the center of robot, i.e. half of track width
-		Wheel wheel1 = WheeledChassis.modelWheel(LEFT_MOTOR, 3.22).offset(-9.2);
-		Wheel wheel2 = WheeledChassis.modelWheel(RIGHT_MOTOR, 3.22).offset(9.2);
+		Wheel wheel1 = WheeledChassis.modelWheel(RIGHT_MOTOR, 3.22).offset(9.2);
+		Wheel wheel2 = WheeledChassis.modelWheel(LEFT_MOTOR, 3.22).offset(-9.2).invert(true);
 		
 		// set up the chassis type, i.e. Differential pilot
 		Chassis chassis = new WheeledChassis(new Wheel[] {wheel1, wheel2},WheeledChassis.TYPE_DIFFERENTIAL);
@@ -37,12 +40,13 @@ public class OctagonMover {
 		pilot.setLinearSpeed(10); // centimeters per second
 		
 		// loop 8 times to trace out a square
+		LCD.clear();
 		for (int sides = 1; sides < 9; sides++) {
 		// travel 50 centimeter
 		LCD.drawString("Side " + sides, 0, sides-1);
 		pilot.travel(50);
 		// rotate 45 degrees
-		LCD.drawString(", turn " + sides, 7, sides-1);
+		LCD.drawString(", turn " + sides, 6, sides-1);
 		pilot.rotate(45);
 		}
 		// press the ESCAPE button to stop moving
@@ -54,7 +58,7 @@ public class OctagonMover {
 		LCD.clear();
 		LCD.drawString("Plotting finished", 0, 4);
 		
-		buttons.waitForAnyPress();
+		Delay.msDelay(2000);
 		LEFT_MOTOR.close();
 		RIGHT_MOTOR.close();
 	}	
