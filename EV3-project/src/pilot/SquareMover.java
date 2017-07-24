00e6519f82242ffc6b5f2e491df603339d3603bb
arqetype <arq@telenet.be>
2017-07-24 11:52:48 +0200

    Made some small changes in display

--- EV3-project/src/pilot/SquareMover.java
+++ EV3-project/src/pilot/SquareMover.java
@@ -10,22 +10,24 @@ import lejos.robotics.chassis.Chassis;
 import lejos.robotics.chassis.Wheel;
 import lejos.robotics.chassis.WheeledChassis;
 import lejos.robotics.navigation.*;
+import lejos.utility.Delay;
 
 public class SquareMover {
 
-	static EV3LargeRegulatedMotor LEFT_MOTOR = new EV3LargeRegulatedMotor(MotorPort.C);
 	static EV3LargeRegulatedMotor RIGHT_MOTOR = new EV3LargeRegulatedMotor(MotorPort.B);
+	static EV3LargeRegulatedMotor LEFT_MOTOR = new EV3LargeRegulatedMotor(MotorPort.C);
 	
 	public static void main(String[] args) throws Exception {
 		EV3 wallE = (EV3) BrickFinder.getLocal();
 		Keys buttons = wallE.getKeys();
-		LCD.drawString("  Plotting out   ", 0, 0);
-		LCD.drawString("    a square     ", 0, 1);
+		LCD.drawString("   Plotting out   ", 0, 2);
+		LCD.drawString("     a square     ", 0, 3);
+		LCD.drawString("taking right turns", 0, 4);
 		
 		// setup the wheel diameter of left (and right) motor in centimeters, i.e. 2.8 cm
 		// the offset number is the distance between the center of wheel to the center of robot, i.e. half of track width
-		Wheel wheel1 = WheeledChassis.modelWheel(LEFT_MOTOR, 3.22).offset(-9.2);
-		Wheel wheel2 = WheeledChassis.modelWheel(RIGHT_MOTOR, 3.22).offset(9.2);
+		Wheel wheel1 = WheeledChassis.modelWheel(RIGHT_MOTOR, 3.22).offset(9.2);
+		Wheel wheel2 = WheeledChassis.modelWheel(LEFT_MOTOR, 3.22).offset(-9.2).invert(true);
 		
 		// set up the chassis type, i.e. Differential pilot
 		Chassis chassis = new WheeledChassis(new Wheel[] {wheel1, wheel2},WheeledChassis.TYPE_DIFFERENTIAL);
@@ -42,16 +44,12 @@ public class SquareMover {
 		// rotate 90 degrees
 		pilot.rotate(90);
 		}
-		// press the ESCAPE button to stop moving
-		while (pilot.isMoving()) {
-		if (buttons.getButtons() == Keys.ID_ESCAPE)
-		pilot.stop();
-		}
+
 		// block the thread until a button is pressed
 		LCD.clear();
-		LCD.drawString(".    Finished    .", 0, 4);
-		buttons.waitForAnyPress();
-		LEFT_MOTOR.close();
+		LCD.drawString("|    Finished    |", 0, 3);
+		Delay.msDelay(3000);
 		RIGHT_MOTOR.close();
+		LEFT_MOTOR.close();
 	}
 }
