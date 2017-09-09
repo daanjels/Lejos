e36b196cc730bdb95a80f6b1c8365e2d8859ea6d
arqetype <arq@telenet.be>
2017-09-09 23:42:00 +0200

    Aligned this with the lejos Delay class

--- TestFiles/utility/Delay.java
+++ TestFiles/utility/Delay.java
@@ -1,11 +1,30 @@
 package utility;
-
+/**
+ * Simple collection of time delay routines that are non interruptable.
+ * @author andy
+ */
 public class Delay {
-	public static void msDelay(int milliseconds) {
-		try {
-			Thread.sleep(milliseconds);
-		} catch (InterruptedException ex) {
-			ex.printStackTrace();
-		}
+	/**
+	 * Wait for the specified number of milliseconds.
+	 * Delays the current thread for the specified period of time. Can not
+	 * be interrupted (but it does preserve the interrupted state).
+	 * @param period time to wait in ms
+	 */
+	public static void msDelay(long period)
+	{
+		if (period <= 0) return;
+		long end = System.currentTimeMillis() + period;
+		boolean interrupted = false;
+		do {
+			try {
+				Thread.sleep(period);
+			} catch (InterruptedException ie)
+			{
+				interrupted = true;
+			}
+			period = end - System.currentTimeMillis();
+		} while (period > 0);
+		if (interrupted)
+			Thread.currentThread().interrupt();
 	}
 }
